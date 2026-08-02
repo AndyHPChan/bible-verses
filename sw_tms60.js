1c1980ddf8d9efce6b7c2ec293ed847df045c904
AndyHPChan <eehpchan@hotmail.com>
2026-08-02 17:53:07 +0800

    Add files via upload

---
+++ sw_tms60.js
@@ -0,0 +1,48 @@
+// Service Worker for TMS 60 Bible Verses PWA
+const CACHE_NAME = 'tms60-v1';
+const urlsToCache = [
+  './tms60_pwa.html',
+  './manifest_tms60.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate and clean up old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
